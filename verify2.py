e809b2e171e338e39378165dc1cb049829e51383
caiyilian <2216098934@qq.com>
2026-06-19 12:42:01 +0800

    .

--- verify2.py
+++
@@ -1,8 +0,0 @@
-import py_compile
-py_compile.compile('app.py', doraise=True)
-print('app.py OK')
-import os
-for f in ['fix_step.py', 'fix_step2.py', 'fix_es.py']:
-    if os.path.exists(f):
-        os.remove(f)
-print('cleaned')
